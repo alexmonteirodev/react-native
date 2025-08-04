e9beccac963b72dc2dfe1e04c8e19327aa6675a8
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-08-04 11:09:58 +0200

    update navbar

---
+++ teste/app/(tabs)/_layout.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "@/constants/icons";
+import TabIcon from "../components/TabIcon";
+
+const _layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} text={"Home"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} text={"Search"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} text={"Saved"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} text={"Profile"} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
---
+++ teste/app/components/TabIcon.tsx
@@ -0,0 +1,37 @@
+import { Text, View, Image, ImageBackground } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+
+const TabIcon = ({
+  focused,
+  icon,
+  text,
+}: {
+  focused: boolean;
+  icon: any;
+  text: string;
+}) => {
+  if (focused) {
+    return (
+      <>
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+          <Image source={icon} tintColor="#151312" className="size-5" />
+          <Text className="text-secondary text-base font-semibold ml-2">
+            {text}
+          </Text>
+        </ImageBackground>
+      </>
+    );
+  } else {
+    return (
+      <View className="size-full justify-center items-center mt-5 rounded-full">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      </View>
+    );
+  }
+};
+
+export default TabIcon;
